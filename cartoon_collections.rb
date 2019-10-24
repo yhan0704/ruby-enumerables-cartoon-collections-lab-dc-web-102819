b18e8ae4b948a5d427596496cb556b98a29f6ba1
Young Han <han20@umbc.edu>
2019-10-24 22:24:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 
-
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
 end
 
