36f6bc04a6fc6b74bdf15280b8d4ccd70942985d
Young Han <han20@umbc.edu>
2019-10-24 22:35:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,10 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |array|
     if array.length < 4
-      
+      true
+    else
+      false
+    end
 end
 
 def find_the_cheese# code an argument here
