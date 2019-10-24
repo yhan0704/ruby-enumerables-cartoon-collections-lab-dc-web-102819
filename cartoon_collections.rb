8c54ee93aa54213b457c7c6e5826cb407504e913
Young Han <han20@umbc.edu>
2019-10-24 22:19:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ dwarves= %w[
             ] 
             
 def roll_call_dwarves(dwarves)# code an argument here
- dwarves.each_with_index{|item, index| "#{index+1}.*#{item}\n"}
+ dwarves.each_with_index{|item, index| 
+ puts "#{index+1}.*#{item}\n"}
 end
 
 def summon_captain_planet# code an argument here
