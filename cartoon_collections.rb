c717fd335e3fa68e2638aa61b84f18884e5d3366
Young Han <han20@umbc.edu>
2019-10-24 22:29:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer)# code an argument here
   planeteer.collect{|x| "#{x.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
 end
 
