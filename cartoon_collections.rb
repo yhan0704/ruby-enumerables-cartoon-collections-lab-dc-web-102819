ccdeceda41ce27cab6f9f823b84479445a733594
Young Han <han20@umbc.edu>
2019-10-24 21:34:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name)# code an argument here
   # Your code here
+  p name
 end
 
 def summon_captain_planet# code an argument here
