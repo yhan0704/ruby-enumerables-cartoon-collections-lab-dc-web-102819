ba7aafeb759ebb5847d7f8181e71ebcbe0920f4b
Young Han <han20@umbc.edu>
2019-10-24 22:48:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,5 @@ end
  cheese_types = ["cheddar", "gouda", "camembert"]
 def find_the_cheese(cheese_types)# code an argument here
   a = "gouda"
-  if cheese_types.include?(a) 
-   return a
+  if cheese_types.include?(a) == true return a 
 end
