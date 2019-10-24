c92d5d87414dc232af05761d763512576a99b504
Young Han <han20@umbc.edu>
2019-10-24 23:00:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,5 @@ end
 
 def find_the_cheese(fruit)# code an argument here
  cheese_types = ["cheddar", "gouda", "camembert"]
-  if fruit.include?(cheese_types)
+  if fruit.include?(cheese_types.each{|x| p x})
 end
